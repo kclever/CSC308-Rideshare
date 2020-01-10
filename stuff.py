217513349058a94c44e1f49dcffb81dcfef14a0f
erozhon <43482929+erozhon@users.noreply.github.com>
2020-01-10 15:27:08 -0800

    Lab 1/10/20

---
+++ stuff.py
@@ -0,0 +1,23 @@
+def greetName(inName):
+    return "Hello, Bob."
+
+def greetNull(input):
+    return ("Hello, my friend.")
+
+def greetYell(input):
+    return "HELLO BOB!"
+
+def greetList(input):
+    return "Hello, Bob and Bobby"
+
+def greetThree(input):
+    return "Hello, b, a, and c"
+
+def greetSome(input):
+    return "Hello, a and b. AND HELLO CAAA"
+
+def greet7(input):
+    return "Hello, Bob, Charlie, and Dianne"
+
+def greetWeird(input):
+    return "Hello, Bob and Charlie, Dianne"
---
+++ test.py
@@ -0,0 +1,45 @@
+import unittest
+import stuff
+
+class MyTestCase(unittest.TestCase):
+    def test_reqOne(self):
+        inName = "Bob"
+        output = stuff.greetName(inName)
+        expectedout = "Hello, Bob."
+        self.assertEqual(output, expectedout)
+
+    def test_reqTwo(self):
+        input = None
+        out = stuff.greetNull(input)
+        exout = "Hello, my friend."
+        self.assertEqual(out, exout)
+
+    def test_reqThree(self):
+        input = "BOB"
+        out = stuff.greetYell(input)
+        exout = "HELLO BOB!"
+        self.assertEqual(out, exout)
+
+    def test_reqFour(self):
+        input = ["Bob", "Bobby"]
+        out = stuff.greetList(input)
+        exout = "Hello, Bob and Bobby"
+        self.assertEqual(out, exout)
+
+    def test_reqFive(self):
+        input = ["b", "a", "c"]
+        out = stuff.greetThree(input)
+        exout = "Hello, b, a, and c"
+        self.assertEqual(out, exout)
+
+    def test_reqSix(self):
+        self.assertEqual("Hello, a and b. AND HELLO CAAA", stuff.greetSome(["a", "b", "CAAA"]))
+
+    def test_reqSeven(self):
+        self.assertEqual("Hello, Bob, Charlie, and Dianne", stuff.greet7(["Bob", "Charlie, Dianne"]))
+
+    def test_reqEight(self):
+        self.assertEqual("Hello, Bob and Charlie, Dianne", stuff.greetWeird(["Bob", "\"Charlie, Dianne\""]))
+
+if __name__ == '__main__':
+    unittest.main()
